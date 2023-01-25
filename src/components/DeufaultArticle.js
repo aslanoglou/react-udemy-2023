d77e1064172d93ea3e75aed7556d66a6d9a6f502
aslanoglou <iordanis.aslanoglou@gmail.com>
2023-01-25 16:16:20 +0200

    business article

--- src/components/DeufaultArticle.js
+++ src/components/DeufaultArticle.js
@@ -1,14 +1,14 @@
 import React from "react";
 const DefaultArticle = (props)=>{
     return(
-    <article className={"column is-4-desktop is-4-tablet is-12-mobile is-relative"}>
-        <figure className={"mb-16"}>
-            <a href={props.url} rel={"noreferrer"} target={"_blank"}>
+    <article className="column is-4-desktop is-4-tablet is-12-mobile is-relative">
+        <figure className="mb-16">
+            <a href={props.url} rel="noreferrer" target="_blank">
                 <img src={props.src} alt={props.alt}/>
             </a>
         </figure>
         <h3 className={"h4 mt-4"}>
-            <a href={props.url} rel={"noreferrer"} target={"_blank"} className={"has-text-black link-overall"}>
+            <a href={props.url} rel="noreferrer" target="_blank" className="has-text-black link-overall">
                 {props.title}
             </a>
         </h3>
